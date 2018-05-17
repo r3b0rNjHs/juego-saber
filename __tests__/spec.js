f3c8a281fd2aa1e445077e0db88f38b550dd2f8d
Jorge Hernández <r3b0rn.jhs@gmail.com>
2018-05-17 14:49:53 +0100

    Merge branch 'master' of https://github.com/carlosble/juego-saber
    # Conflicts:
    #	__tests__/spec.js
    #	package.json
    #	views/index.pug

--- __tests__/spec.js
+++ __tests__/spec.js
@@ -29,28 +29,6 @@ describe("the game", function(){
            document.getElementsByClassName('btnStart'))
            .not.toBeNull();
    });
-   xit('should press start button', function (done) {
-        // This test is not going to work because jsdom does not implement
-        // the MutationObserver object. It would work with a real browser.
-
-        let buttonStart = document.getElementById('buttonStart');
-        console.log(buttonStart.classList.toggle('invisible'));
-        buttonStart.click();
-        let questionsBox = document.getElementById('questions');
-        var config = { attributes: true, childList: true };
-        var callback = function(mutationsList) {
-            let answer = document.getElementById('3');
-            answer.click();
-            let dale = document.getElementById('btn');
-            dale.click();
-            let score = document.getElementById('scoreUI');
-            expect(score.innerText).toBe('2');
-            done();
-        };
-        var observer = new MutationObserver(callback);
-        observer.observe(questionsBox, config);
-        observer.disconnect();
-   });
     it('should hide btnStart when is clicked', function(){
         let buttonStart = document.getElementById('buttonStart');
         console.log(buttonStart.classList.toggle('invisible'));
